a8a3a658f6d67d70cc8ee28a9f7b68c63d55abca
Alex Garrett-Smith <alex@codecourse.com>
2023-11-27 17:26:47 +0000

    All code

---
+++ app/History/Traits/Historyable.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\History\Traits;
+
+use App\History;
+use Illuminate\Support\Arr;
+use App\History\ColumnChange;
+use Illuminate\Database\Eloquent\Model;
+
+trait Historyable
+{
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public static function bootHistoryable()
+    {
+        static::updated(function (Model $model) {
+            collect($model->getWantedChangedColumns($model))->each(function ($change) use ($model) {
+                $model->saveChange($change);
+            });
+        });
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param ColumnChange $change
+     * @return void
+     */
+    protected function saveChange(ColumnChange $change)
+    {   
+        $this->history()->create([
+            'changed_column' => $change->column,
+            'changed_value_from' => $change->from,
+            'changed_value_to' => $change->to,
+        ]);
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param Model $model
+     * @return void
+     */
+    protected function getWantedChangedColumns(Model $model)
+    {
+        return collect(
+            array_diff(
+                Arr::except($model->getChanges(), $this->ignoreHistoryColumns()),
+                $original = $model->getOriginal()
+            )
+        )
+            ->map(function ($change, $column) use ($original) {
+                return new ColumnChange($column, Arr::get($original, $column), $change);
+            });
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function history()
+    {
+        return $this->morphMany(History::class, 'historyable')
+            ->oldest();
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @return void
+     */
+    public function ignoreHistoryColumns()
+    {
+        return [
+            'updated_at'
+        ];
+    }
+}
